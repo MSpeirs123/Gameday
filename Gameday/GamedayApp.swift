337809cb178d48cd1e0f2b102255b619a8d4f310
Matthew Speirs <speirsmatthew@yahoo.co.uk>
2025-02-10 10:34:53 -0500

    Initial Commit

---
+++ Gameday/GamedayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamedayApp.swift
+//  Gameday
+//
+//  Created by Matthew Speirs on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GamedayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
